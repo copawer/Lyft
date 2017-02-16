9d81827e117d913e4c5dd2a959355f9bdfbdf3ea
Sara Gabriela Díaz Oporto <sara.diaz.oporto@gmail.com>
2017-02-16 15:18:58 -0500

    Lyft

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var map;
+/*var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
@@ -21,4 +21,71 @@ function initMap() {
     map: map,
     title: 'aqui es'
   });
-}
+}*/
+var marca;
+var coorde={};
+
+ initMap =function(){
+     navigator.geolocalizacion.getCurrenPosition(
+         function(posicion){
+             coorde ={
+                 log:posicion.coorde.longuitud,
+                 lat:posicion.coorde.latitud
+             };
+             setMapa(coorde);
+         },function(Error){console.log(error);});
+     
+ }
+ //country
+ var country='[{"nombre": "Perú","img":,+ "flags-iso/flat/16/PE.png",+ "phone_code": 51 }]'
+ 
+ 
+     
+     
+    /* {
+   "nombre": "Australia",
+    "img": "flags-iso/flat/16/AU.png",
+    "phone_code": 61
+  },
+  {
+    "nombre": "Canadá",
+    "img": "flags-iso/flat/16/CA.png",
+    "phone_code": 1
+  },
+  {
+    "nombre": "China",
+    "img":"flags-iso/flat/16/CN.png" ,
+    "phone_code": 86
+  },
+  {
+    "nombre": "Francia",
+    "img": "flags-iso/flat/16/FR.png",
+    "phone_code": 33
+  },
+  {
+    "nombre": "India",
+    "img":"flags-iso/flat/16/IN.png" ,
+    "phone_code": 91
+  },
+  {
+    "nombre": "Japón",
+    "img": "flags-iso/flat/16/JP.png",
+    "phone_code": 81
+  },
+  {
+    "nombre": "México",
+    "img":"flags-iso/flat/16/MX.png" ,
+    "phone_code": 52
+  },
+     {
+    "nombre": "Puerto Rico",
+    "img": "flags-iso/flat/16/PR.png",
+    "phone_code": 1
+  },
+     {
+    "nombre": "Reino Unido",
+    "img": "flags-iso/flat/16/GB.png",
+    "phone_code": 44
+  },
+     ]*/
+ 
